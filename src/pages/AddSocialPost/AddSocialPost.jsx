7357d8fedc9e95a496ef3bdf08e90e5d7bb741da
byamane <byamane21@gmail.com>
2022-03-25 11:05:58 -0500

    set initial state for AddSocialPost form & stub up render

--- src/pages/AddSocialPost/AddSocialPost.jsx
+++ src/pages/AddSocialPost/AddSocialPost.jsx
@@ -1,11 +1,32 @@
+import { useState, useRef, useEffect } from "react"
+
 const AddSocialPost = (props) => {
+  const formElement = useRef()
+  const [formData, setFormData] = useState({
+    content: ''
+  })  
   
-  
-  return ( 
-  
-  <h1>
-    Sup
-  </h1> );
+  return (
+  <>
+    <h1>Add Post</h1>
+      <form autoComplete="off">
+        <div>
+          <textarea 
+            type="text"
+            id="name-input"
+            name="content"
+            value={formData.content}
+            required
+          />
+        </div>
+      <button 
+        type="submit"
+      >
+        Submit Post
+      </button>
+    </form>
+  </>
+  );
 }
  
 export default AddSocialPost;
