362c1669c9e39cc88487d09a9d4c58bcc4242309
byamane <byamane21@gmail.com>
2022-03-25 12:30:22 -0500

    add handleChange & checkValidity to Add Social Post form

--- src/pages/AddSocialPost/AddSocialPost.jsx
+++ src/pages/AddSocialPost/AddSocialPost.jsx
@@ -2,25 +2,38 @@ import { useState, useRef, useEffect } from "react"
 
 const AddSocialPost = (props) => {
   const formElement = useRef()
+
   const [formData, setFormData] = useState({
     content: ''
-  })  
+  })
+
+  const [validForm, setValidForm] = useState(false)
+  
+  const handleChange = evt => {
+		setFormData({ ...formData, [evt.target.name]: evt.target.value })
+	}
+
+  useEffect(() => {
+		formElement.current.checkValidity() ? setValidForm(true) : setValidForm(false)
+	}, [formData])
   
   return (
   <>
     <h1>Add Post</h1>
-      <form autoComplete="off">
+      <form autoComplete="off" ref={formElement}>
         <div>
           <textarea 
             type="text"
             id="name-input"
             name="content"
             value={formData.content}
+            onChange={handleChange}
             required
           />
         </div>
       <button 
         type="submit"
+        disabled={!validForm}
       >
         Submit Post
       </button>
