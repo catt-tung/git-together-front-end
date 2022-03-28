de6dba6cc31d679f16c02bb106ffdc2dfa5ce946
byamane <byamane21@gmail.com>
2022-03-28 09:42:08 -0500

    add collapsible comments section

--- src/components/AddComment/AddComment.jsx
+++ src/components/AddComment/AddComment.jsx
@@ -1,12 +1,25 @@
-const AddComment = () => {
-  return ( 
-    <>
-      <button>Add Comment</button>
-      {/* <button onClick={() => handleComment(post._id)}>&times;</button> */}
-   
-    </>
+import React, { useState } from 'react'
+import useCollapse from 'react-collapsed'
 
-   );
+
+const AddComment = () => {
+  const [isExpanded, setExpanded] = useState(false)
+  const { getCollapseProps, getToggleProps } = useCollapse({ isExpanded })
+
+  return (
+    <>
+      <div>
+        <button
+          {...getToggleProps({
+            onClick: () => setExpanded((prevExpanded) => !prevExpanded),
+          })}
+        >
+          {isExpanded ? 'Collapse' : 'All Comments'}
+        </button>
+        <section {...getCollapseProps()}>Collapsed comments here!</section>
+      </div>
+    </>
+  )
 }
  
 export default AddComment;
