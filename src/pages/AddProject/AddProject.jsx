05e87e5611bfeb8715128824c5fdbc39e62b8381
Catherine Tung <catt.tung@gmail.com>
2022-03-29 17:11:11 -0400

    Add state to goal/addproject

--- src/pages/AddProject/AddProject.jsx
+++ src/pages/AddProject/AddProject.jsx
@@ -1,11 +1,19 @@
 import React, { useState, useEffect, useRef } from 'react';
 import AddGoal from "../../components/Goals/Goals";
+import * as goalService from '../../services/project'
 
 const AddProject = () => {
+  const [goals, setGoals] = useState([])
+
+  useEffect(() => {
+    goalService.getGoals()
+    .then(goalsData => setGoals(goalsData))
+  }, [])
+
   return ( 
     <>
       <h1>Page to Add New Projects</h1>
-      <AddGoal />
+      <AddGoal goals={goals} />
     </>
     
   );
