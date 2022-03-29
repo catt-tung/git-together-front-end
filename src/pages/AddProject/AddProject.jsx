c86fc1725c183813379cdbea970ea0ec9a20e7ea
Catherine Tung <catt.tung@gmail.com>
2022-03-29 17:44:58 -0400

    Add state to goals

--- src/pages/AddProject/AddProject.jsx
+++ src/pages/AddProject/AddProject.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import AddGoal from "../../components/Goals/Goals";
 import * as goalService from '../../services/project'
 
