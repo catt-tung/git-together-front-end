34a2ab440ba2733060f0f86523d4eb120645b839
Catherine Tung <catt.tung@gmail.com>
2022-03-30 17:28:55 -0400

    Display some project details

--- src/pages/MyProjectDetails/MyProjectDetails.jsx
+++ src/pages/MyProjectDetails/MyProjectDetails.jsx
@@ -6,16 +6,18 @@ import { useLocation } from "react-router-dom";
 
 const MyProjectDetails = (props) => {
   const location = useLocation()
-  const [goals, setGoals] = useState([])
-  console.log(location)
-  useEffect(() => {
-    projectService.getGoals()
-    .then(goalsData => setGoals(goalsData))
-  }, [])
+  // const [project, setProject] = useState([])
+
+  // useEffect(() => {
+  //   projectService.getProjectDetails(location.state.project._id)
+  //   .then(projectData => setProject(projectData))
+  // }, [])
 
   return ( 
     <>
       <h1>Project Details Component</h1>
+      <h3>{location.state.project.repo}</h3>
+      <h5>Projected Completion Date: {new Date(location.state.project.completionDate).toLocaleDateString()}</h5>
       <AddGoal projectid={location.state.project._id} />
     </>
   );
