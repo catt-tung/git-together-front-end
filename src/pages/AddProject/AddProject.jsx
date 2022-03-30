6af3b3671137c89abfed889dc5fcf0ffe51b6644
Catherine Tung <catt.tung@gmail.com>
2022-03-30 02:00:33 -0400

    Add project form

--- src/pages/AddProject/AddProject.jsx
+++ src/pages/AddProject/AddProject.jsx
@@ -13,7 +13,53 @@ const AddProject = () => {
   return ( 
     <>
       <h1>Page to Add New Projects</h1>
-      <AddGoal goals={goals} />
+      <h1>Create a New Project</h1>
+      <form autoComplete="off">
+				<div className="form-group mb-3">
+					<label htmlFor="name-input" className="form-label">
+						Select a repository:
+					</label>
+					<input 
+						type="select"
+						className="form-control"
+						id="repo-input"
+						name="repo"
+						required
+					/>
+				</div>
+        <div className="form-group mb-4">
+					<label htmlFor="age-input" className="form-label">
+						Upload a photo
+					</label>
+					<input 
+						type="text"
+						className="form-control"
+						id="age-input"
+						name="age"
+					/>
+				</div>
+				<div className="form-group mb-3">
+					<label htmlFor="breed-input" className="form-label">
+						Completion date goal:
+					</label>
+					<input 
+						type="date"
+						className="form-control"
+						id="date-input"
+						name="completionDate"
+						required
+					/>
+				</div>
+				<div className="d-grid">
+					<button
+						type="submit"
+						className="btn btn-primary btn-fluid"
+					>
+						Create Project
+					</button>
+				</div>
+			</form>
+      {/* <AddGoal goals={goals} /> */}
     </>
     
   );
