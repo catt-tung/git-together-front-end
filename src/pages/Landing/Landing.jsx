a8c3606b10793706f7d516a9dea157d6ed544845
byamane <byamane21@gmail.com>
2022-03-31 10:58:56 -0500

    adjust landing page for Github avatar

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,9 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import { getDetails } from '../../services/profileService';
 import styles from './Landing.module.css'
 
 const Landing = ({ user }) => {
+  const [profile, setProfile] = useState([])
+
+  useEffect(() => {
+    getDetails(user.profile)
+    .then(profile => setProfile(profile))
+  }, [])
+
+  const avatar = `https://github.com/${profile.gitUser}.png`
+
   return (
     <main className={styles.container}>
-      <h1>hello, {user ? user.name : 'friend'}</h1>
+      <h1>Hello, {user ? user.name : 'friend'}</h1>
+      <img src={avatar} alt="Your GitHub Profile Pic"></img>
+      
     </main>
   )
 }
