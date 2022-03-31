74187459f4b4955ea4620a3a5fae290445464936
byamane <byamane21@gmail.com>
2022-03-31 12:35:59 -0500

    revert landing page for Chris' changes

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,24 +1,9 @@
-import { getDetails } from '../../services/profileService';
 import styles from './Landing.module.css'
 
 const Landing = ({ user }) => {
-  let profile = {}
-  let avatar = ""
-
-  if (user) {
-    profile = getDetails(user.profile)
-    avatar = `https://github.com/${profile.gitUser}.png`
-    console.log(avatar)
-    console.log(profile)
-  }
-
-  
-    
   return (
     <main className={styles.container}>
-      <h1>Hello, {user ? user.name : 'friend'}</h1>
-      <img src={user ? avatar : "https://cdn-icons-png.flaticon.com/512/889/889192.png"} alt="Your GitHub Profile Pic"></img>
-      
+      <h1>hello, {user ? user.name : 'friend'}</h1>
     </main>
   )
 }
