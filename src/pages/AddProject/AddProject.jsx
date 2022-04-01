ed3b311038453ecbae1ed4983ae69b07bd144315
Catherine Tung <catt.tung@gmail.com>
2022-03-31 22:57:30 -0400

    styling for new project view

--- src/pages/AddProject/AddProject.jsx
+++ src/pages/AddProject/AddProject.jsx
@@ -3,6 +3,7 @@ import AddGoal from "../../components/Goals/Goals";
 import { getGoals, getRepos, createProject } from '../../services/project'
 import { useNavigate } from 'react-router-dom';
 import { getDetails } from '../../services/profileService';
+import './AddProject.css'
 
 
 const AddProject = ({ user }) => {
@@ -60,71 +61,70 @@ const AddProject = ({ user }) => {
 
   return ( 
     <>
-      <h1>Page to Add New Projects</h1>
       <h1>Create a New Project</h1>
-      <form autoComplete="off" ref={formElement} onSubmit={handleSubmit}>
-				<div className="form-group mb-3">
-					<label htmlFor="name-input" className="form-label">
-            Project Name:
-					</label>
-					<input 
-						type="test"
-						className="form-control"
-						id="name-input"
-						name="name"
-            value={formData.name}
-            onChange={handleChange}
-						required
-					/>
-					<label htmlFor="repo-input" className="form-label">Choose Repository:</label>
-						<select name="repo" id="repo-input" value={formData.repo} onChange={handleChange}>
-						{repos.map(repo => 
-							<option >
-							{repo}	
-							</option>
-							)}
-							</select>
-				</div>
-        <div className="form-group mb-4">
-					<label htmlFor="photo-input" className="form-label">
-						Enter an image link
-					</label>
-					<input 
-						type="text"
-						className="form-control"
-						id="image-input"
-						name="image"
-            value={formData.image}
-            onChange={handleChange}
-					/>
-				</div>
-				<div className="form-group mb-3">
-					<label htmlFor="breed-input" className="form-label">
-						Completion date goal:
-					</label>
-					<input 
-						type="date"
-						className="form-control"
-						id="date-input"
-						name="completionDate"
-            value={formData.completionDate}
-            onChange={handleChange}
-						required
-					/>
-				</div>
-				<div className="d-grid">
-					<button
-						type="submit"
-						className="btn btn-primary btn-fluid"
-            disabled={!validForm}
-					>
-						Create Project
-					</button>
-				</div>
-			</form>
-      {/* <AddGoal goals={goals} /> */}
+			<div id="add-project-form-container">
+				<form autoComplete="off" ref={formElement} onSubmit={handleSubmit}>
+					<div className="form-group mb-3">
+						<label htmlFor="name-input" className="form-label">
+							Project Name:
+						</label>
+						<input 
+							type="test"
+							className="form-control"
+							id="name-input"
+							name="name"
+							value={formData.name}
+							onChange={handleChange}
+							required
+						/>
+						<label htmlFor="repo-input" className="form-label">Choose Repository:</label>
+							<select name="repo" id="repo-input" value={formData.repo} onChange={handleChange}>
+							{repos.map(repo => 
+								<option >
+								{repo}	
+								</option>
+								)}
+								</select>
+					</div>
+					<div className="form-group mb-4">
+						<label htmlFor="photo-input" className="form-label">
+							Enter an image link
+						</label>
+						<input 
+							type="text"
+							className="form-control"
+							id="image-input"
+							name="image"
+							value={formData.image}
+							onChange={handleChange}
+						/>
+					</div>
+					<div className="form-group mb-3">
+						<label htmlFor="breed-input" className="form-label">
+							Completion date goal:
+						</label>
+						<input 
+							type="date"
+							className="form-control"
+							id="date-input"
+							name="completionDate"
+							value={formData.completionDate}
+							onChange={handleChange}
+							required
+						/>
+					</div>
+					<div className="d-grid">
+						<button
+							type="submit"
+							className="btn btn-primary btn-fluid"
+							disabled={!validForm}
+						>
+							Create Project
+						</button>
+					</div>
+				</form>
+			</div>
     </>
-    
   );
 }
 
