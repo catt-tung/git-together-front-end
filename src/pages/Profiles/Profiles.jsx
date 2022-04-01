a2c136139290c8f19f239dacbd7493628d9f4cb1
Catherine Tung <catt.tung@gmail.com>
2022-03-31 23:42:24 -0400

    Finish profile styling

--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -27,40 +27,42 @@ const Profiles = ({user}) => {
   return (
     <>
       <h1>All Profiles</h1>
-      {profiles.length ? 
-        <>
-          {profiles.map(profile =>
-          profile._id === userProfile._id ?
-            <div key={profile._id}>
-              <section className="profile-card-on-profiles-page">
-                <Link className='profile-link-on-profiles-page'
-                to="/myProjects"
-                state={{profile}}
-                >
-                  <img className="profile-image-on-profiles-page" src={profile.avatar ? profile.avatar : "https://cdn-icons-png.flaticon.com/512/889/889192.png"} alt="" />
-                  {profile.name}
-                </Link>
-              </section>
-            </div>
-            :
-            <div key={profile._id}>
-              <section className="profile-card-on-profiles-page">
-                
-                <Link
-                className='profile-link-on-profiles-page'
-                to="/profile"
-                state={{profile}}
-                >
-                  <img className="profile-image-on-profiles-page" src={profile.avatar ? profile.avatar : "https://cdn-icons-png.flaticon.com/512/889/889192.png"} alt="" />
-                  {profile.name}
-                </Link>
-              </section>
-            </div>
-            )}
-            </> 
+      <section id="all-profiles">
+        {profiles.length ? 
+          <>
+            {profiles.map(profile =>
+            profile._id === userProfile._id ?
+              <div key={profile._id}>
+                <section className="profile-card-on-profiles-page">
+                  <Link className='profile-link-on-profiles-page'
+                  to="/myProjects"
+                  state={{profile}}
+                  >
+                    <img className="profile-image-on-profiles-page" src={profile.avatar ? profile.avatar : "https://cdn-icons-png.flaticon.com/512/889/889192.png"} alt="" />
+                    {profile.name}
+                  </Link>
+                </section>
+              </div>
               :
-            <p>No profiles yet</p>
-      }
+              <div key={profile._id}>
+                <section className="profile-card-on-profiles-page">
+                  
+                  <Link
+                  className='profile-link-on-profiles-page'
+                  to="/profile"
+                  state={{profile}}
+                  >
+                    <img className="profile-image-on-profiles-page" src={profile.avatar ? profile.avatar : "https://cdn-icons-png.flaticon.com/512/889/889192.png"} alt="" />
+                    {profile.name}
+                  </Link>
+                </section>
+              </div>
+              )}
+              </> 
+                :
+              <p>No profiles yet</p>
+        }
+      </section>
     </>
   )
 }
